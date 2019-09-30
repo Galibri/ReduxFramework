27912c9e0b2cfe8cfb03e0a1808f558bd7948199
Asadullah Al Galib <galib.bcse@gmail.com>
2019-10-01 01:08:04 +0600

    Initial Commit

---
+++ ReduxCore/templates/panel/header.tpl.php
@@ -0,0 +1,73 @@
+<?php
+    /**
+     * The template for the panel header area.
+     * Override this template by specifying the path where it is stored (templates_path) in your Redux config.
+     *
+     * @author      Redux Framework
+     * @package     ReduxFramework/Templates
+     * @version:    3.5.4.18
+     */
+
+    $tip_title = __( 'Developer Mode Enabled', 'redux-framework' );
+
+    if ( $this->parent->dev_mode_forced ) {
+        $is_debug     = false;
+        $is_localhost = false;
+
+        $debug_bit = '';
+        if ( Redux_Helpers::isWpDebug() ) {
+            $is_debug  = true;
+            $debug_bit = __( 'WP_DEBUG is enabled', 'redux-framework' );
+        }
+
+        $localhost_bit = '';
+        if ( Redux_Helpers::isLocalHost() ) {
+            $is_localhost  = true;
+            $localhost_bit = __( 'you are working in a localhost environment', 'redux-framework' );
+        }
+
+        $conjunction_bit = '';
+        if ( $is_localhost && $is_debug ) {
+            $conjunction_bit = ' ' . __( 'and', 'redux-framework' ) . ' ';
+        }
+
+        $tip_msg = __( 'This has been automatically enabled because', 'redux-framework' ) . ' ' . $debug_bit . $conjunction_bit . $localhost_bit . '.';
+    } else {
+        $tip_msg = __( 'If you are not a developer, your theme/plugin author shipped with developer mode enabled. Contact them directly to fix it.', 'redux-framework' );
+    }
+
+?>
+<div id="redux-header">
+    <?php if ( ! empty( $this->parent->args['display_name'] ) ) { ?>
+        <div class="display_header">
+            
+            <h2><?php echo wp_kses_post( $this->parent->args['display_name'] ); ?></h2>
+
+            <?php if ( ! empty( $this->parent->args['display_version'] ) ) { ?>
+                <span><?php echo wp_kses_post( $this->parent->args['display_version'] ); ?></span>
+            <?php } ?>
+
+        </div>
+
+        <div class="backstrap-redux-save-box">
+            <div class="redux-action_bar">
+                <span class="spinner"></span>
+                <?php 
+                    if ( false === $this->parent->args['hide_save'] ) {
+                        submit_button( esc_attr__( 'Save Changes', 'redux-framework' ), 'primary', 'redux_save_sticky', false );
+                        echo '&nbsp';
+                    }
+                    
+                    if ( false === $this->parent->args['hide_reset'] ) {
+                        submit_button( esc_attr__( 'Reset Section', 'redux-framework' ), 'secondary', $this->parent->args['opt_name'] . '[defaults-section]', false, array( 'id' => 'redux-defaults-section-sticky' ) );
+                        echo '&nbsp';
+                        submit_button( esc_attr__( 'Reset All', 'redux-framework' ), 'secondary', $this->parent->args['opt_name'] . '[defaults]', false, array( 'id' => 'redux-defaults-sticky' ) );
+                    }
+                ?>
+            </div>
+        </div>
+        
+    <?php } ?>
+
+    <div class="clear"></div>
+</div>
---
+++ ReduxCore/templates/panel/header_stickybar.tpl.php
@@ -0,0 +1,23 @@
+<?php
+    /**
+     * The template for the header sticky bar.
+     * Override this template by specifying the path where it is stored (templates_path) in your Redux config.
+     *
+     * @author        Redux Framework
+     * @package       ReduxFramework/Templates
+     * @version:      3.6.10
+     */
+?>
+<div id="redux-sticky">
+    <div id="info_bar">
+        
+        <div class="clear"></div>
+    </div>
+
+    <!-- Notification bar -->
+    <div id="redux_notification_bar">
+        <?php $this->notification_bar(); ?>
+    </div>
+
+
+</div>
